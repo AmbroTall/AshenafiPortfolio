c48415305ff663f365cfe3826984e02c17e771e9
Ambrose <ambrosetall@gmail.com>
2023-08-16 16:12:03 +0300

    Adding Icons, education, hobbies and download and view resume pdf

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, download_pdf, display_pdf
 
 app_name = "app"
 urlpatterns = [
     path('', home_page, name="home"),
+    path('download_pdf/', download_pdf, name='download_pdf'),
+    path('view_pdf/', display_pdf , name='view_pdf'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
+
+file_name = "AmbroseNdone.pdf"
+def download_pdf(request):
+    # Path to the PDF file you want to serve for download
+    pdf_file_path = f"/home/ambrose/PycharmProjects/portfolioAsh/portfolio/static/static/{file_name}"
+
+    if os.path.exists(pdf_file_path):
+        with open(pdf_file_path, 'rb') as pdf_file:
+            pdf_content = pdf_file.read()
+            response = HttpResponse(pdf_content, content_type='application/pdf')
+            response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+            return response
+    else:
+        return HttpResponse("PDF file not found.", status=404)
+
+
+def display_pdf(request):
+    # Path to the PDF file you want to display
+    pdf_file_path = f"/home/ambrose/PycharmProjects/portfolioAsh/portfolio/static/static/{file_name}"
+
+    if os.path.exists(pdf_file_path):
+        with open(pdf_file_path, 'rb') as pdf_file:
+            pdf_content = pdf_file.read()
+            response = HttpResponse(pdf_content, content_type='application/pdf')
+            response['Content-Disposition'] = f'inline; filename="{file_name}"'
+            return response
+    else:
+        return HttpResponse("PDF file not found.", status=404)
 
 # Create your views here.
 def home_page(request):
